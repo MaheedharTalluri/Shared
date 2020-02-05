1abb297e20446620d614e3d39f8d6c65780bd0b6
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 19:11:13 +0530

    library files added

--- vars/renamejob.groovy
+++ vars/renamejob.groovy
@@ -1,3 +1,3 @@
 def call(){
-curl -XPOST http://18.219.210.127:8080/job/jobname/doRename?newName=renamed -u admin:119767fb81f22e2f10d8594e4201717e53
+sh "curl -XPOST http://18.219.210.127:8080/job/jobname/doRename?newName=renamed -u admin:119767fb81f22e2f10d8594e4201717e53"
 }
