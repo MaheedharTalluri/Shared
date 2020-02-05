71f5a48b465fbbf730477d71b52ba6e3de39c4af
MaheedharTalluri <1210815148@gitam.in>
2020-02-06 00:23:06 +0530

    connector stages added

--- vars/getsystemconfig.groovy
+++ vars/getsystemconfig.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh "curl -o http://18.219.210.127:8080/computer/ -u admin:119767fb81f22e2f10d8594e4201717e53"
+sh "curl -o http://18.219.210.127:8080/systemInfo -u admin:119767fb81f22e2f10d8594e4201717e53"
 }
