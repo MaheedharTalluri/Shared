9a62920106eddd6fec6872a8e9b0e7481898a89b
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 23:50:08 +0530

    connector stages added

--- vars/addcreds.groovy
+++ vars/addcreds.groovy
@@ -1,7 +1,7 @@
 def call(){
 
 sh "curl -X POST http://admin:119767fb81f22e2f10d8594e4201717e53@18.219.210.127:8080/credentials/store/system/domain/_/createCredentials \
---data-urlencode 'json={ \
+--data-urlencode json={ \
   "": "0", \
   "credentials": { \
     "scope": "GLOBAL", \
@@ -10,5 +10,5 @@ sh "curl -X POST http://admin:119767fb81f22e2f10d8594e4201717e53@18.219.210.127:
     "password": "password_here", \
     "$class": "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl" \
   } \
-}' "
+} "
 }
