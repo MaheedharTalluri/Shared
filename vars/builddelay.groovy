24401dc35e22421ae7aa17d78e1ad432944c8ed0
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 15:44:17 +0530

    library files added

--- vars/builddelay.groovy
+++ vars/builddelay.groovy
@@ -1,3 +1,3 @@
 def call(jobname){
-sh "curl -X POST http://18.219.210.127:8080/job/'${jobname}'/build?delay=10sec -u admin:119767fb81f22e2f10d8594e4201717e53"
+sh "curl -X POST http://18.219.210.127:8080/job/'${jobname}'/build?delay=100sec -u admin:119767fb81f22e2f10d8594e4201717e53"
 }
