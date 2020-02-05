5f8f729a16f954b556682483ef64b52eea5bd2c2
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 23:39:24 +0530

    connector stages added

--- vars/addcreds.groovy
+++ vars/addcreds.groovy
@@ -1,8 +1,7 @@
 def call(){
 
 sh "curl -X POST http://admin:119767fb81f22e2f10d8594e4201717e53@18.219.210.127:8080/credentials/store/system/domain/_/createCredentials \
--H "Expect:" \
--H 'Content-Type: application/json; charset=utf-8' \
+
 --data-urlencode 'json={ \
   "": "0", \
   "credentials": { \
