6c60eb9eb010f1657426c96cac84012c203172f1
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 17:19:21 +0530

    library files added

--- vars/deletebuilds.groovy
+++ vars/deletebuilds.groovy
@@ -1,4 +1,4 @@
 def call(jobname,buildrange){
 sh "curl -O http://18.219.210.127:8080/jnlpJars/jenkins-cli.jar"
-sh "java -jar jenkins-cli.jar -s http://18.219.210.127:8080 delete-builds '${jobname}' ''${buildrange}'' --username admin --password:119767fb81f22e2f10d8594e4201717e53"
+sh "java -jar jenkins-cli.jar -s http://18.219.210.127:8080 -auth admin:119767fb81f22e2f10d8594e4201717e53 delete-builds '${jobname}' ''${buildrange}''"
 }
