ddd81cf93c5c5a0e63b6126fbfb18cf9ccf800a3
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 23:36:02 +0530

    connector stages added

--- vars/addcreds.groovy
+++ vars/addcreds.groovy
@@ -3,17 +3,15 @@ def call(){
 sh "curl -X POST http://admin:119767fb81f22e2f10d8594e4201717e53@18.219.210.127:8080/credentials/store/system/domain/_/createCredentials \
 -H "Expect:" \
 -H 'Content-Type: application/json; charset=utf-8' \
---data-binary @- << EOF \
-{ \
-  \"\": \"0\", \
-  \"credentials\": { \
-    \"scope\": \"GLOBAL\", \
-    \"id\": \"apicredentias\", \
-    \"username\": \"apicredentials\", \
-    \"password\": \"pass\", \
-    \"description\": \"apicredentials\", \
-    \"stapler-class\": \"com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl\" \
-  } \
-} \
-EOF "
+--data-urlencode 'json={
+  "": "0",
+  "credentials": {
+    "scope": "GLOBAL",
+    "id": "credential_id_here",
+    "username": "username_here",
+    "password": "password_here",
+    "description": "My new credentials",
+    "$class": "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl"
+  }
+}' "
 }
