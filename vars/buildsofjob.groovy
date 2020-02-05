530197111682c85ee08b885c6b53914ffe105015
MaheedharTalluri <1210815148@gitam.in>
2020-02-05 19:41:40 +0530

    library files added

--- vars/buildsofjob.groovy
+++ vars/buildsofjob.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh "curl -XGET http://18.219.210.127:8080/job/paratest/api/json?tree=builds[number,status,timestamp,id,result] -u admin:119767fb81f22e2f10d8594e4201717e53"
+sh "curl -XGET -g http://18.219.210.127:8080/job/paratest/api/json?tree=builds[number,status,timestamp,id,result] -u admin:119767fb81f22e2f10d8594e4201717e53"
 }
