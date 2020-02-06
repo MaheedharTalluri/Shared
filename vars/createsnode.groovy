18ac4b05fc9e563803629dae55c7fa3af9970189
MaheedharTalluri <1210815148@gitam.in>
2020-02-06 15:43:43 +0530

    user add

--- vars/createsnode.groovy
+++ vars/createsnode.groovy
@@ -1,4 +1,4 @@
 def call(){
 echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("user1", "password123")' |
-java -jar jenkins-cli.jar -s http://http://18.219.210.127:8080 groovy =
+java -jar jenkins-cli.jar -s 'http://18.219.210.127:8080' groovy =
 }
