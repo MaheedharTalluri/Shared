55c50fc853566fa732c7e713b8baf16e37a3d9f0
MaheedharTalluri <58215374+MaheedharTalluri@users.noreply.github.com>
2020-03-02 14:46:00 +0530

    Update username.groovy

--- vars/username.groovy
+++ vars/username.groovy
@@ -53,7 +53,7 @@ def resultJson = jsonSlurper.parse(reader)
 def call()
 {
     sh "curl -XGET -g http://52.14.229.175:8080/asynchPeople/api/json?depth=3 -o username.json"
-	sh "touch user.json" 
+	sh "sudo nano user.json" 
 	
     create()
 	
