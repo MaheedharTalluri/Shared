2e93562641c961119867556f389d8ee413f89b21
MaheedharTalluri <58215374+MaheedharTalluri@users.noreply.github.com>
2020-03-02 15:17:36 +0530

    Update username.groovy

--- vars/username.groovy
+++ vars/username.groovy
@@ -40,13 +40,7 @@ def resultJson = jsonSlurper.parse(reader)
      println("total number of builds by vicky:"+countv) 
     println("total number of success builds by vicky:"+success_vicky) 
      println("total number of failed builds by vicky:"+fail_vicky) 
-	writeFile file: 'user.json', text: '{ \
-	\"name\": \"Suneel\", \
-	\"Builds\": \"'${countS}'\", \
-	\"Successful\": \"'${success_sunil}'\", \
-	\"Failed\": \"'${fail_sunil}'\" \
-}'
-sh 'ls -l user.json'
+	
 	
 }
      
@@ -57,7 +51,13 @@ def call()
 	sh 'echo "{" >> user.json' 
 	
     create()
-	
+	writeFile file: 'user.json', text: '{ \
+	\"name\": \"Suneel\", \
+	\"Builds\": \"'${countS}'\", \
+	\"Successful\": \"'${success_sunil}'\", \
+	\"Failed\": \"'${fail_sunil}'\" \
+}'
+sh 'ls -l user.json'
 	/*sh 'echo """{ \
 	\"name\": \"Suneel\", \
 	\"Builds\": \"'${countS}'\", \
